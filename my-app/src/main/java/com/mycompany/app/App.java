a8377ed088b8d0c44065ed4ee54d90a609e9baa9
Riccardo Tacconi <riccardo.tacconi@workday.com>
2015-02-03 14:21:19 +0000

    First coomit

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+import static spark.Spark.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
